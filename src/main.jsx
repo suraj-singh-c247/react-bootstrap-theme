1200e1e9fe20603865b45b75d70569c49a01af8e
tusharv-c247 <tushar.v@chapter247.com>
2025-12-11 15:09:34 +0530

    update/mixing-logic & implement light-dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+// import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 import "./styles/bootstrap-custom.scss"; // Your custom SCSS overrides
 
